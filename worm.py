c45676dee1dbff0c558211f0eec43c6162dc8599
amgbb <amgbb@outlook.com>
2015-03-16 08:38:26 -0500

    Starting coding.

---
+++ worm.py
@@ -0,0 +1,10 @@
+"""
+worm.py - an implementation of a worm, for academic purposes for course
+INFO3155 Information Assurance and Security at UWI, Dept. of Computing.
+"""
+
+def main():
+    pass
+
+#if __name__ == '__main__':
+#   main()
