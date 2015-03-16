01a57f92b02d6d95f01625950bc0839b3db2850d
amgbb <amgbb@outlook.com>
2015-03-16 11:52:33 -0500

    Outlining structure.

--- worm.py
+++ worm.py
@@ -1,9 +1,25 @@
 """
 worm.py - an implementation of a worm, for academic purposes for course
 INFO3155 Information Assurance and Security at UWI, Dept. of Computing.
+This file is either to be used on a system with a Python environment, or
+made into a standalone executable. It should ideally loaded onto a target
+and executed somehow. When main() runs it should perform 3 basic tasks:
+monitor and sendback resource usage of target system to some remote system,
+replication, and propagation. We should be able to disable the replication/
+propagation functions both at build and in use. We should also have a
+reliable method of stopping the program at any time.
 """
 
 def main():
+    """Main function of program."""
+    pass
+
+def repl():
+    """Replication function."""
+    pass
+
+def prop():
+    """Propagation function."""
     pass
 
 #if __name__ == '__main__':
