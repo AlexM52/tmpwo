2f97fefabdf8d6ee76a8e0022cda3b2eaaa4ff8e
alexm52 <alexmcdoom@hotmail.com>
2015-03-18 07:44:57 +0000

    Started on a server element to receive report messages and possibly dispatch commands.

---
+++ base.py
@@ -0,0 +1,20 @@
+import socket
+
+def main():
+    print "Starting process...\n Creating socket..."
+    srvsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "Binding socket.."
+    srvsock.bind((socket.gethostname(), 8080))
+    srvsock.listen(2)
+    print "Listening.\n"
+    
+    #loop
+    while True:
+        (c, addr) = srvsock.accept()
+        print "Connection from : ", addr
+        msg = c.recv(4096)
+        print msg
+        c.close()
+        srvsock.close()
+    
+main()
